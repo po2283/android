855bbd4c4c429a047366316d87ba7b25a1ff9dff
Ji won Na <jiwonna52@gmail.com>
2022-05-30 17:04:06 +0900

    Spinner애서 고른 값을 버튼을 누르면 반영되도록 하게 했다.
    'id'에다가 아이디를 치고 'spinner1'에다가 주소를 선택해서 버튼을 누르면 밑에 텍스트 뷰에서 값이 나오는 기능이다.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,24 +21,59 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
         Spinner spinner = findViewById(R.id.spinner2);
         TextView txt = findViewById(R.id.txtspinner);
+        EditText edtID = findViewById(R.id.ID);
+        Button bt = findViewById(R.id.button2);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.item, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
-        txt.setText(spinner.getSelectedItem().toString());
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText(edtID.getText().toString() + '@' + spinner.getSelectedItem().toString());
+            }
+        });
+
+
+/**
+ *         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+ *             @Override
+ *             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+ *                 txt.setText(edtID.getText().toString() + spinner.getSelectedItem().toString());
+ *             }
+ *
+ *             @Override
+ *             public void onNothingSelected(AdapterView<?> parent) {
+ *
+ *             }
+ *         });
+ */
+
+
+
+
+        //txt.setText(spinner.getSelectedItem().toString());
 
 
     }
 
-    public void onBtnClick (View view){
-        TextView txt = findViewById(R.id.textView);
+    /**
+     *
+     * @param view
+     *           public void onBtnClick (View view){
+     *               TextView txt = findViewById(R.id.textView);
+     *
+     *               EditText edtID = findViewById(R.id.ID);
+     *
+     *               txt.setText(edtID.getText().toString() + '@');
+     *
+     *
+     *         }
+     */
 
-        EditText edtID = findViewById(R.id.ID);
-        EditText edtAddress = findViewById(R.id.Address);
-
-        txt.setText(edtID.getText().toString() + '@' + edtAddress.getText().toString());
 
 
-    }
 
 }
